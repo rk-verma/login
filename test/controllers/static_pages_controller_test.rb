ce4f229f41ed1e24039db64762cb221541407538
Rajat Verma <rkvsfu@icloud.com>
2015-10-18 20:32:33 -0700

    Started home, help and About Us pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "HELP"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "ABOUT"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "HOME"
   end
 
 end
